aa3354f9ea95521082127248e49704cd263b644c
Firred <adri-ankenobi@hotmail.com>
2019-04-11 19:09:17 +0200

    Añadido cruce por ciclos.

--- Practica_1/src/common/cruce/CruceCiclos.java
+++ Practica_1/src/common/cruce/CruceCiclos.java
@@ -1,13 +1,23 @@
 package common.cruce;
 
-import common.Poblacion;
+import common.Cromosoma;
 
 public class CruceCiclos extends Reproduccion {
 	
 	@Override
 	ParCromosoma cruce(ParCromosoma par) {
-		// TODO Auto-generated method stub
-		return null;
+		int pos = 0;
+		
+		Cromosoma h1 = new Cromosoma(par.getC2()), h2 = new Cromosoma(par.getC1());
+		
+		while (par.getC1().getGen(0) != par.getC2().getGen(pos)) {
+			h1.getGenes().set(pos, par.getC1().getGen(pos));
+			h2.getGenes().set(pos, par.getC2().getGen(pos));
+			
+			pos = par.getC1().getGenes().indexOf(par.getC2().getGen(pos));
+		}
+		
+		return new ParCromosoma(h1, h2);
 	}
 
 }
